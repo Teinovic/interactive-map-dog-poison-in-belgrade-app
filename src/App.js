dc082da2af8403b99498870cf41c92e4755179ea
Teinovic <momir.teinovic.2011@gmail.com>
2021-08-24 23:44:43 +0200

    started building the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+/* global document */
+import * as React from 'react';
+import {useState} from 'react';
+import {render} from 'react-dom';
+import MapGL from 'react-map-gl';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoibW9taXIiLCJhIjoiY2tzcWxmb3ZtMGR4cjJ2bzM0bjM5ZG5lNyJ9.QT_n1D2H-nL1RxhbcyycRA'; // Set your mapbox token here
+
+function Root() {
+  const [viewport, setViewport] = useState({
+    latitude: 37.8,
+    longitude: -122.4,
+    zoom: 14,
+    bearing: 0,
+    pitch: 0
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapGL
+      {...viewport}
+      width="100vw"
+      height="100vh"
+      mapStyle="mapbox://styles/mapbox/dark-v9"
+      onViewportChange={setViewport}
+      mapboxApiAccessToken={MAPBOX_TOKEN}
+    />
   );
 }
 
-export default App;
+document.body.style.margin = 0;
+render(<Root />, document.body.appendChild(document.createElement('div')));
